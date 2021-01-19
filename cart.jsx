3abed5e23fa2ec9818e4ecb277ce61532d67fec1
mamuka-gulbani <65565771+mamuka-gulbani@users.noreply.github.com>
2021-01-19 06:07:04 -0800

    Add files via upload

---
+++ cart.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import products from './components/products/products'
+
+export default class cart extends Component {
+    /* aq ragac rom unda gavaketo vici,ogond ra ver vxvdebi*/
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
